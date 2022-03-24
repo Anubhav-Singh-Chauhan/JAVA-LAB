6b535c5e530a504b0186fdc106e6fe5d9dd8a833
Anubhav <chauhananubhav0011@gmail.com>
2022-03-24 19:49:01 +0530

    practical-2 Task-1

---
+++ src/P2_Task01_KeyWordsInJava.java
@@ -0,0 +1,19 @@
+public class P2_Task01_KeyWordsInJava {
+    public static void main(String[] args) {
+        double int = 9.98d; //Syntax Error, Invalid Variable Declaration, we can't use Keyword as variable name
+
+    }
+
+    void int() //Cannot Use Keyword as function name
+    {
+//        .....
+//        .....
+    }
+
+    Class Student;
+    {
+        //We also not supposed to use Keyword as Object name in java. For e.g. -> Student int = 2;
+    }
+}
+
+
