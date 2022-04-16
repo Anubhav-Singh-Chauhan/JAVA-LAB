af7682f0b79d1694989f025e794343b8f725ca9d
Anubhav <chauhananubhav0011@gmail.com>
2022-04-16 23:12:14 +0530

    Sample Class

---
+++ src/Unit_01/SampleClass.java
@@ -0,0 +1,13 @@
+package Unit_01;
+
+
+public class SampleClass{
+    int a;
+     public int DisplayFunc(int x){
+         System.out.println("Sample Class Display Function");
+         a=x;
+         return a;
+     }
+
+}
+
