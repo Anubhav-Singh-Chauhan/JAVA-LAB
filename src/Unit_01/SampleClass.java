3c570faa1b59cafc27b6ea4bc483ff49a19549e1
Anubhav <chauhananubhav0011@gmail.com>
2022-05-09 21:57:19 +0530

    Sample Class

--- src/Unit_01/SampleClass.java
+++ src/Unit_01/SampleClass.java
@@ -2,7 +2,7 @@ package Unit_01;
 
 
 public class SampleClass{
-    int a;
+    public int a;
      public int DisplayFunc(int x){
          System.out.println("Sample Class Display Function");
          a=x;
